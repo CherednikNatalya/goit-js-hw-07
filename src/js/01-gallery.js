6f33b904f8364f67bc43318e79072307b0b128d5
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-10-03 22:42:09 +0300

    комит

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer =document.querySelector('.gallery');
+const galleryMarkup = createGalleryRef(galleryItems)
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryContainer.addEventListener('click', onContainerClick)
+function createGalleryRef(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery-item">
+    <a class="gallery-link" href ="${original}">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      data-source="${original}"
+      ></a>
+  </div>`;
+})
+.join('');
+}
+
+function onContainerClick (evt) {
+evt.preventDefault();
+
+if(evt.target.nodeName !== "IMG"){
+    return;
+}
+console.log(evt.target);
+
+const instance = basicLightbox.create(`
+    <img src="${galleryItems.original}" width="800" height="600">
+`)
+
+instance.show()
+galleryContainer.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+})
+instance.close() 
+
+}
+
+
+
+
+
+
+
+
