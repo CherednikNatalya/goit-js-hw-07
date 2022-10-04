801e36de90b66db7a644d8d6e7ec8e39a73d4677
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2022-10-04 14:05:29 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,7 +14,7 @@ galleryContainer.addEventListener('click', onContainerClick)
 
 function createGalleryRef(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
-        return `<a class="gallery__item" href="${preview}">
+        return `<a class="gallery__item" href="${original}">
         <img 
         class="gallery__image" 
         src="${preview}" 
